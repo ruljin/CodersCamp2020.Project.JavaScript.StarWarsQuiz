5968e5a75651adedc7726e1e4577c306e66ff58d
Suegro24 <marciu199@gmail.com>
2021-01-02 12:43:49 +0100

    Deleted files with extentions .css, .css.map, BEM corrects, add main tag

--- src/pages/authors/authors.js
+++ src/pages/authors/authors.js
@@ -1,22 +1,23 @@
 import '../../styles/assets.scss';
-import './authors.scss'
+import './authors.scss';
 
 const buttonSkip = document.querySelector('#buttonSkip');
-const containerStarWarsEffect = document.querySelector('.container--star-wars-effect');
-const containerAuthors = document.querySelector('.container--authors');
+const containerStarWarsEffect = document.querySelector(
+  '.container-star-wars-effect'
+);
+const containerAuthors = document.querySelector('.container-authors');
 const ANIMATION_DURATION = 60;
 
 const checkAnimationFinished = () => {
-    setTimeout(() => {
-        skipAnimation();
-    }, ANIMATION_DURATION * 1000 )
-}
+  setTimeout(() => {
+    skipAnimation();
+  }, ANIMATION_DURATION * 1000);
+};
 
 const skipAnimation = () => {
-    containerStarWarsEffect.style.display = 'none';
-    containerAuthors.style.display = 'flex';
-}
+  containerStarWarsEffect.style.display = 'none';
+  containerAuthors.style.display = 'flex';
+};
 
 window.addEventListener('load', checkAnimationFinished, false);
 buttonSkip.addEventListener('click', skipAnimation, false);
-
