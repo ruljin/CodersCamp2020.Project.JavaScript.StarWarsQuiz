1a44eb97b87afddbca82825fe945da8d68e3e25c
Suegro24 <marciu199@gmail.com>
2021-01-02 15:26:17 +0100

    BEM changes

--- src/pages/authors/authors.js
+++ src/pages/authors/authors.js
@@ -5,7 +5,7 @@ const buttonSkip = document.querySelector('#buttonSkip');
 const containerStarWarsEffect = document.querySelector(
   '.container__star-wars-effect'
 );
-const containerAuthors = document.querySelector('.container__authors');
+const containerAuthors = document.querySelector('.authors');
 const ANIMATION_DURATION = 60;
 
 const checkAnimationFinished = () => {
