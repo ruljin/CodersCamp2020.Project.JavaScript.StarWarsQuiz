a83fe1c9dd67d006cd1e34bc750d3cd91046d4b2
Mrozelek <46111864+Mrozelek@users.noreply.github.com>
2021-01-03 15:18:23 +0100

    Remove merge leftovers

--- src/pages/game-over/game-over.js
+++ src/pages/game-over/game-over.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-import '../../styles/assets.scss';
-import './game-over.scss';
-=======
 import '../../styles/assets.scss'
 import './game-over.scss'
->>>>>>> 179e629163807f340956e06b34d4d6408d695d2f
