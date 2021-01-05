3e287fdf48d8a2ebd7ebfb2e6669b263ce37f446
mrozelek <mrozelek@gmail.com>
2021-01-06 00:42:14 +0100

    Join common code from webpack configs to one file

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,10 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const config = require('./config.js');
+const merge = require('webpack-merge');
+const commonConfig = require('./webpack.config.common');
 const path = require('path');
 
-const createHtmlWebpackPlugin = siteName =>
-  new HtmlWebpackPlugin({
-    template: `./src/pages/${siteName}/${siteName}.html`,
-    inject: 'head',
-    scriptLoading: 'defer',
-    chunks: [`${siteName}`],
-    filename: `${siteName}.html`
-  });
-
-module.exports = {
-  entry: Object.fromEntries(
-    config.SITES.map(siteName => [
-      siteName,
-      path.resolve(__dirname, `./src/pages/${siteName}/${siteName}.js`)
-    ])
-  ),
+module.exports = merge.merge(commonConfig, {
+  mode: 'production',
   output: {
     path: path.resolve(__dirname, 'docs')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource'
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-              sassOptions: {
-                outputStyle: 'compressed'
-              }
-            }
-          }
-        ]
-      }
-    ]
-  },
-  plugins: config.SITES.map(createHtmlWebpackPlugin)
-};
+  }
+});
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,59 +1,9 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const config = require('./config.js');
-const path = require('path');
+const merge = require('webpack-merge');
+const commonConfig = require('./webpack.config.common');
 
-const createHtmlWebpackPlugin = siteName =>
-  new HtmlWebpackPlugin({
-    template: `./src/pages/${siteName}/${siteName}.html`,
-    inject: 'head',
-    scriptLoading: 'defer',
-    chunks: [`${siteName}`],
-    filename: `${siteName}.html`
-  });
-
-module.exports = {
-  entry: Object.fromEntries(
-    config.SITES.map(siteName => [
-      siteName,
-      path.resolve(__dirname, `./src/pages/${siteName}/${siteName}.js`)
-    ])
-  ),
+module.exports = merge.merge(commonConfig, {
+  mode: 'development',
   devServer: {
     port: 8080
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource'
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-              sassOptions: {
-                outputStyle: 'compressed'
-              }
-            }
-          }
-        ]
-      }
-    ]
-  },
-  plugins: config.SITES.map(createHtmlWebpackPlugin)
-};
+  }
+});
