456c0df1f4ef8ba2d0ba55d599b18a42d16722bb
mrozelek <mrozelek@gmail.com>
2021-01-08 00:24:46 +0100

    Add favicon and fix img

--- src/pages/game/game.js
+++ src/pages/game/game.js
@@ -1,2 +1,7 @@
 import '../../styles/assets.scss';
 import './game.scss';
+import sampleImage from '../../assets/img/modes/people/1.jpg';
+import lightSaberImage from '../../assets/ui/lightsaberHandle.png';
+
+document.querySelector('#questionImage').src = sampleImage;
+document.querySelector('#lightSaberImage').src = lightSaberImage;
