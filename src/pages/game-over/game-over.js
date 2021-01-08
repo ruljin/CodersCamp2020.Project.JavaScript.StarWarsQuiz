8534dac6cabc5b4cdf29f43b03d668cfb836035a
Weronika Brzęczkowska <brzeczkowska.w@gmail.com>
2021-01-08 15:57:12 +0100

    solo mode added and functions created

--- src/pages/game-over/game-over.js
+++ src/pages/game-over/game-over.js
@@ -1,58 +1,78 @@
 import '../../styles/assets.scss';
 import './game-over.scss';
 
-const input = document.querySelector('.input');
-const playerName = document.querySelector('.table--player-name');
+const localStorage = require('../../scripts/localScorage.js');
+
 const header = document.querySelector('.container__header');
 const firstPlayer = document.querySelector('.table--first-player');
 const firstScore = document.querySelector('.table--first-score');
 const secondPlayer = document.querySelector('.table--second-player');
 const secondScore = document.querySelector('.table--second-score');
+const second = document.querySelector('.table--second');
 const firstRow = document.querySelector('.table--first');
 const secondRow = document.querySelector('.table--second');
 
-let playerScore = 45;
-let computerScore = 55;
-//const playerScore = localStorage.getItem('getPlayerCorrectAnswersNumber');
-//const computerScore = localStorage.getItem('getComputerCorrectAnswersNumber');
-
-
-
+let playerScore = 35;
+let computerScore = 18;
+//const playerScore = localStorage.getPlayerCorrectAnswersNumber;
+//const computerScore = localStorage.getComputerCorrectAnswersNumber;
 
 //Set game over or you win
-if (playerScore >= computerScore) {
-    header.innerText = "You Won!";
-    header.style.color = "#70b5f5";
+function headerText() {
+    if (playerScore >= computerScore) {
+        header.innerText = "You Won!";
+        header.style.color = "#70b5f5";
+    }
+    else {
+        header.innerText = "Game over!";
+        header.style.color = "#ff0000";
+    }
 };
 
 //Set first score on first place and highlighte player
-if (playerScore >= computerScore) {
-    firstPlayer.innerText = "Player";
-    firstScore.innerText = playerScore;
-    firstRow.setAttribute("class", "table__row--highlighted"); 
-    firstPlayer.setAttribute("class", "table--player-name");
-}
-else {
-    firstPlayer.innerText = "Computer";
-    firstScore.innerText = computerScore;
-}
+function firstPlace() {
+    if (playerScore >= computerScore) {
+        firstPlayer.innerText = "Player";
+        firstScore.innerText = playerScore;
+        firstRow.setAttribute("class", "table__row--highlighted"); 
+    }
+    else {
+        firstPlayer.innerText = "Computer";
+        firstScore.innerText = computerScore;
+    }
+};
 
 //Set second score on second place and highlighte player
-if (computerScore > playerScore) {
-    secondPlayer.innerText = "Player";
-    secondScore.innerText = playerScore;
-    secondRow.setAttribute("class", "table__row--highlighted");
-    secondPlayer.setAttribute("class", "table--player-name");
-}
-else {
-    secondPlayer.innerText = "Computer";
-    secondScore.innerText = computerScore;
+function secondPlace() {
+    if (computerScore > playerScore) {
+        secondPlayer.innerText = "Player";
+        secondScore.innerText = playerScore;
+        secondRow.setAttribute("class", "table__row--highlighted");
+    }
+    else {
+        secondPlayer.innerText = "Computer";
+        secondScore.innerText = computerScore;
+    }
+};
+
+//Solo player mode 
+function soloPlayer() {
+    if (playerScore && !computerScore) {
+        header.innerText = "You Won!";
+        header.style.color = "#70b5f5";
+        firstPlayer.innerText = "Player";
+        firstScore.innerText = playerScore;
+        firstRow.setAttribute("class", "table__row--highlighted"); 
+        second.innerText = "--";
+    }
+    else {
+        console.log(headerText());
+        console.log(firstPlace());
+        console.log(secondPlace());
+    }
 }
 
-//Change "player" for input player's name in table 
-input.addEventListener('input', name => {
-    playerName.textContent = name.target.value
-});
+console.log(soloPlayer());
 
 
 
