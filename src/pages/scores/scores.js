564849ac2d98ab8f2b954ba71efdf37b45e285e6
Ada Krupa <adriannakrupa01@gmail.com>
2021-01-09 18:35:50 +0100

    Add change table

--- src/pages/scores/scores.js
+++ src/pages/scores/scores.js
@@ -2,15 +2,83 @@ import '../../styles/assets.scss';
 import './scores.scss';
 
 const localStorage = require('../../scripts/localScorage');
-
 const selectorOne = document.querySelector('.selector:nth-of-type(1)');
-
 const table = document.querySelector('.table');
+const scoreboard = localStorage.getScoreboard();
+
+function peopleScores() {
+  scoreboard.forEach(function (element) {
+    if (element.category === 'people') {
+      table.innerHTML = `
+      <thead class="table__head">
+      <tr class="table__row">
+        <th class="table__data">Place</th>
+        <th class="table__data">Player</th>
+        <th class="table__data">Points</th>
+      </tr>
+    </thead>
+    <tbody>
+        <tr class="table__row">
+          <td class="table__data">1st</td>
+          <td class="table__data">${element.name}</td>
+          <td class="table__data">${element.points}</td>
+        </tr>
+      </tbody>
+      `;
+    }
+  });
+}
+
+function vehiclesScores() {
+  scoreboard.forEach(function (element) {
+    if (element.category === 'vehicles') {
+      table.innerHTML = `
+      <thead class="table__head">
+      <tr class="table__row">
+        <th class="table__data">Place</th>
+        <th class="table__data">Player</th>
+        <th class="table__data">Points</th>
+      </tr>
+    </thead>
+    <tbody>
+        <tr class="table__row">
+          <td class="table__data">1st</td>
+          <td class="table__data">${element.name}</td>
+          <td class="table__data">${element.points}</td>
+        </tr>
+      </tbody>
+      `;
+    }
+  });
+}
+
+function starshipsScores() {
+  scoreboard.forEach(function (element) {
+    if (element.category === 'starships') {
+      table.innerHTML = `
+      <thead class="table__head">
+      <tr class="table__row">
+        <th class="table__data">Place</th>
+        <th class="table__data">Player</th>
+        <th class="table__data">Points</th>
+      </tr>
+    </thead>
+    <tbody>
+        <tr class="table__row">
+          <td class="table__data">1st</td>
+          <td class="table__data">${element.name}</td>
+          <td class="table__data">${element.points}</td>
+        </tr>
+      </tbody>
+      `;
+    }
+  });
+}
 
 // select default category
 window.onload = function () {
   selectorOne.classList.add('selector--selected');
-  table.style.backgroundColor = 'red';
+  peopleScores();
 };
 
 //Change category after click
@@ -36,14 +104,14 @@ btns.forEach(function (element) {
       element.classList.contains('selector--people') &&
       element.classList.contains('selector--selected')
     ) {
-      table.style.backgroundColor = 'red';
+      peopleScores();
     } else if (
       element.classList.contains('selector--vehicles') &&
       element.classList.contains('selector--selected')
     ) {
-      table.style.backgroundColor = 'blue';
+      vehiclesScores();
     } else {
-      table.style.backgroundColor = 'green';
+      starshipsScores();
     }
   });
 });
