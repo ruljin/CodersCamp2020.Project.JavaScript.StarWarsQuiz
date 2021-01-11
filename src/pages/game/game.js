240ebf3b068ad6a5512145e18a8f0eb6dbfe30f7
mrozelek <mrozelek@gmail.com>
2021-01-11 10:44:08 +0100

    Add whitespaces

--- src/pages/game/game.js
+++ src/pages/game/game.js
@@ -1,7 +1,9 @@
 import '../../styles/assets.scss';
 import './game.scss';
 import './game.timer';
+
 import sampleImage from '../../assets/img/modes/people/1.jpg';
-import lightSaberImage from '../../assets/ui/lightsaberHandle.png';
 document.querySelector('#questionImage').src = sampleImage;
+
+import lightSaberImage from '../../assets/ui/lightsaberHandle.png';
 document.querySelector('#lightSaberImage').src = lightSaberImage;
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,7 +1,6 @@
 import '../../styles/assets.scss';
 import './index.scss';
+import './modal.js';
 
 import sampleImage from '../../assets/img/modes/people/1.jpg';
 document.querySelector('#questionImage').src = sampleImage;
-
-import './modal.js';
